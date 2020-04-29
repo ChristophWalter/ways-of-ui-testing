e2f84c06026592f2bc1c2e9ffe4bdffb5a170a7b
ChristophWalter <ChristophWalter@users.noreply.github.com>
2020-04-29 11:43:16 +0200

    Setup demo functionality: open wizard and select radio button

---
+++ src/components/TheWizard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="wizard">
+    <div class="wizard__content">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi
+        eos exercitationem impedit in magni nisi reprehenderit velit voluptate
+        voluptatibus! Architecto deserunt facere itaque maiores optio quidem quo
+        reiciendis vitae.
+      </p>
+      <fieldset class="fieldset">
+        <input
+          type="radio"
+          id="try"
+          name="Mode"
+          value="try"
+          :checked="selectedMode === 'try'"
+          @click="selectedMode = 'try'"
+        />
+        <label for="try"> Try it first</label><br />
+        <input
+          type="radio"
+          id="buy"
+          name="Mode"
+          value="buy"
+          :checked="selectedMode === 'buy'"
+          @click="selectedMode = 'buy'"
+        />
+        <label for="buy"> Buy it straight away</label>
+      </fieldset>
+      <div class="actions">
+        <button @click="$emit('close')">CLOSE</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TheWizard",
+  data() {
+    return {
+      selectedMode: undefined,
+    }
+  },
+  props: {
+    mode: String,
+  },
+  mounted() {
+    this.selectedMode = this.mode
+  },
+}
+</script>
+<style scoped>
+.wizard {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.wizard__content {
+  background: #fff;
+  padding: 50px;
+  max-width: 700px;
+}
+.fieldset {
+  text-align: left;
+}
+.actions {
+  margin-top: 20px;
+  text-align: right;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    <HelloWorld />
+  <div class="home">
+    <h1>Welcome to this special landing page.</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorem
+      enim ipsa odit tempora. Deleniti facilis illo molestiae placeat sequi
+      similique temporibus. Ad consequatur dolorem eius et obcaecati provident,
+      quo?
+    </p>
+    <button @click="openWizard('try')">TRY</button>
+    <button @click="openWizard('buy')" class="buy">BUY</button>
+    <TheWizard v-if="mode" :mode="mode" @close="mode = null" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue"
+import TheWizard from "@/components/TheWizard"
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    TheWizard,
+  },
+  data() {
+    return {
+      mode: undefined,
+    }
+  },
+  methods: {
+    openWizard(mode) {
+      this.mode = mode
+    },
   },
 }
 </script>
+<style scoped>
+.home {
+  max-width: 700px;
+  margin: 0 auto;
+  padding: 50px 0;
+}
+.buy {
+  margin-left: 30px;
+}
+</style>
