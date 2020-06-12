8f20e8e8900e7320a1204b13d8c8cd9dda36a8e9
ChristophWalter <ChristophWalter@users.noreply.github.com>
2020-06-12 12:44:30 +0200

    Update Vue Testing Library and avoid common mistakes

--- tests/unit/PackageSelection.vtl.spec.js
+++ tests/unit/PackageSelection.vtl.spec.js
@@ -4,16 +4,16 @@ import { render, fireEvent } from "@testing-library/vue"
 
 it("should select the correct package", async () => {
   const DIALOG_CONTENT = "Thanks for your interest!"
-  const { getByText, queryByText, getByLabelText } = render(Home, {}, vue => {
+  const { getByText, queryByText, getByRole } = render(Home, {}, vue => {
     vue.use(VueFormWizard)
   })
-  expect(queryByText(DIALOG_CONTENT)).toBeNull()
-  await fireEvent.click(getByText("BUY"))
-  getByText(DIALOG_CONTENT)
-  expect(getByLabelText("Buy it straight away")).toHaveAttribute(
+  expect(queryByText(DIALOG_CONTENT)).not.toBeInTheDocument()
+  await fireEvent.click(getByRole("button", { name: "BUY" }))
+  expect(getByText(DIALOG_CONTENT)).toBeInTheDocument()
+  expect(getByRole("radio", { name: "Buy it straight away" })).toHaveAttribute(
     "aria-selected",
     "true"
   )
-  await fireEvent.click(getByLabelText("Cancel"))
-  expect(queryByText(DIALOG_CONTENT)).toBeNull()
+  await fireEvent.click(getByRole("button", { name: "Cancel" }))
+  expect(queryByText(DIALOG_CONTENT)).not.toBeInTheDocument()
 })
