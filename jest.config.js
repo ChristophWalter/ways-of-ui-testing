3dbeebbc37057ab966f8269d71fa9dabc598b72c
ChristophWalter <ChristophWalter@users.noreply.github.com>
2020-06-12 17:27:13 +0200

    Reorganize tests to separate based on its framework

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
-  setupFilesAfterEnv: ['./jest.setup.js']
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  testMatch: ["**/tests/**/*.spec.js"],
 }
